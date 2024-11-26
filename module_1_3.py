809bae99cc826076fb58d13b0b4df5a5b715fb66
VictorTerentev <130257882+VictorTerentev@users.noreply.github.com>
2024-11-26 05:37:45 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,14 @@
+name = 'Viktor'
+print("Name: " + name)
+
+age = 21
+print("Age: " + str(age))
+
+age = age + 12
+print("New Age: " + str(age))
+
+is_student = True
+print("Is Student: " + str(is_student))
+
+
+
