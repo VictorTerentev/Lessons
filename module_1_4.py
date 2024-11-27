c9a9aabb6df6ee4621ebf88a0e99e3af31b808fe
VictorTerentev <130257882+VictorTerentev@users.noreply.github.com>
2024-11-27 08:16:58 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+
+quantity_of_homework = 12
+quantity_of_hours = 1.5
+urban_course = 'Python'
+time_for_one_homework = 1.5 / 12
+
+print(f'Курс: {urban_course}, всего задач: {quantity_of_homework}, затрачено часов: {quantity_of_hours}, среднее время '
+      f'выполнения {time_for_one_homework} часа')
+
+
