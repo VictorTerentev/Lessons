285552ae5e65a8e925b59bda985efee2633347d2
VictorTerentev <130257882+VictorTerentev@users.noreply.github.com>
2024-12-04 07:55:26 +0300

    Add files via upload

---
+++ module_1_5_2.py
@@ -0,0 +1,13 @@
+immutable_var = (1, "String", True)
+print(immutable_var)
+
+immutable_var[0] = 2
+immutable_var[1] = 'Hello'
+immutable_var[2] = False
+print(immutable_var)
+
+mutable_list = [5, "Viktor", False]
+mutable_list[0] = 61
+mutable_list[1] = 'mutable_list'
+mutable_list[2] = True
+print(mutable_list)
